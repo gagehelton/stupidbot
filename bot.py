c0ff9c3405a4efc9ff2c918c56dee18413a935a3
mghelton <gagehelton@gmail.com>
2021-01-09 04:03:56 +0000

    stupidbot

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import random
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+@client.command()
+async def jasonism(ctx):
+    jasonisms = ['Sixteenth chapel',
+    'Take it for granite',
+    'More often than none',
+    'Mind bottling',
+    'Rerecurring']
+    await ctx.send(random.choice(jasonisms))
+
+client.run('Nzk3MzA5NzMzMzcxMjQ4Njkx.X_kmjQ.tkBDdEQPa5Nci3tOstxCbIhzCiY')
